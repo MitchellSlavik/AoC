60d85df3e8378e5d48bdef3ecad5cc596f4f7b13
Mitchell Slavik <mitchell.slavik@aviture.us.com>
2023-12-14 00:36:43 -0600

    Clean up day 14

--- aoc2023/src/day14/index.ts
+++ aoc2023/src/day14/index.ts
@@ -2,206 +2,135 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
-const rollRocksUp = (lines: string[]): [boolean, string[]] => {
-  const newLines = [...lines];
-  let movedSomething = false;
-
-  for (let y = 0; y < newLines.length; y++) {
-    for (let x = 0; x < newLines[y].length; x++) {
-      if (newLines[y].charAt(x) === "O") {
-        if (y > 0 && newLines[y - 1].charAt(x) === ".") {
-          newLines[y - 1] =
-            newLines[y - 1].substring(0, x) +
-            "O" +
-            newLines[y - 1].substring(x + 1);
-          newLines[y] =
-            newLines[y].substring(0, x) + "." + newLines[y].substring(x + 1);
-          movedSomething = true;
+const rollRocksNorth = (lines: string[]) => {
+  for (let x = 0; x < lines[0].length; x++) {
+    let movedSomething;
+    do {
+      movedSomething = false;
+      for (let y = 0; y < lines.length; y++) {
+        if (lines[y].charAt(x) === "O") {
+          if (y > 0 && lines[y - 1].charAt(x) === ".") {
+            lines[y - 1] =
+              lines[y - 1].substring(0, x) +
+              "O" +
+              lines[y - 1].substring(x + 1);
+            lines[y] =
+              lines[y].substring(0, x) + "." + lines[y].substring(x + 1);
+            movedSomething = true;
+          }
         }
       }
-    }
+    } while (movedSomething);
   }
-
-  return [movedSomething, newLines];
 };
 
-const rollRocksDown = (lines: string[]): [boolean, string[]] => {
-  const newLines = [...lines];
-  let movedSomething = false;
-
-  for (let y = 0; y < newLines.length; y++) {
-    for (let x = 0; x < newLines[y].length; x++) {
-      if (newLines[y].charAt(x) === "O") {
-        if (y < newLines.length - 1 && newLines[y + 1].charAt(x) === ".") {
-          newLines[y + 1] =
-            newLines[y + 1].substring(0, x) +
-            "O" +
-            newLines[y + 1].substring(x + 1);
-          newLines[y] =
-            newLines[y].substring(0, x) + "." + newLines[y].substring(x + 1);
-          movedSomething = true;
+const rollRocksSouth = (lines: string[]) => {
+  for (let x = 0; x < lines[0].length; x++) {
+    let movedSomething;
+    do {
+      movedSomething = false;
+      for (let y = lines.length - 1; y >= 0; y--) {
+        if (lines[y].charAt(x) === "O") {
+          if (y < lines.length - 1 && lines[y + 1].charAt(x) === ".") {
+            lines[y + 1] =
+              lines[y + 1].substring(0, x) +
+              "O" +
+              lines[y + 1].substring(x + 1);
+            lines[y] =
+              lines[y].substring(0, x) + "." + lines[y].substring(x + 1);
+            movedSomething = true;
+          }
         }
       }
-    }
+    } while (movedSomething);
   }
-
-  return [movedSomething, newLines];
 };
 
-const rollRocksEast = (lines: string[]): [boolean, string[]] => {
-  const newLines = [...lines];
-  let movedSomething = false;
-
-  for (let y = 0; y < newLines.length; y++) {
-    for (let x = 0; x < newLines[y].length; x++) {
-      if (newLines[y].charAt(x) === "O") {
-        if (x < newLines[y].length - 1 && newLines[y].charAt(x + 1) === ".") {
-          newLines[y] =
-            newLines[y].substring(0, x) + ".O" + newLines[y].substring(x + 2);
-          movedSomething = true;
+const rollRocksEast = (lines: string[]) => {
+  for (let y = 0; y < lines.length; y++) {
+    let movedSomething;
+    do {
+      movedSomething = false;
+      for (let x = lines[y].length; x >= 0; x--) {
+        if (lines[y].charAt(x) === "O") {
+          if (x < lines[y].length - 1 && lines[y].charAt(x + 1) === ".") {
+            lines[y] =
+              lines[y].substring(0, x) + ".O" + lines[y].substring(x + 2);
+            movedSomething = true;
+          }
         }
       }
-    }
+    } while (movedSomething);
   }
-
-  return [movedSomething, newLines];
 };
 
-const rollRocksWest = (lines: string[]): [boolean, string[]] => {
-  const newLines = [...lines];
-  let movedSomething = false;
-
-  for (let y = 0; y < newLines.length; y++) {
-    for (let x = 0; x < newLines[y].length; x++) {
-      if (newLines[y].charAt(x) === "O") {
-        if (x > 0 && newLines[y].charAt(x - 1) === ".") {
-          newLines[y] =
-            newLines[y].substring(0, x - 1) +
-            "O." +
-            newLines[y].substring(x + 1);
-          movedSomething = true;
+const rollRocksWest = (lines: string[]) => {
+  for (let y = 0; y < lines.length; y++) {
+    let movedSomething;
+    do {
+      movedSomething = false;
+      for (let x = 0; x < lines[y].length; x++) {
+        if (lines[y].charAt(x) === "O") {
+          if (x > 0 && lines[y].charAt(x - 1) === ".") {
+            lines[y] =
+              lines[y].substring(0, x - 1) + "O." + lines[y].substring(x + 1);
+            movedSomething = true;
+          }
         }
       }
-    }
+    } while (movedSomething);
   }
-
-  return [movedSomething, newLines];
 };
+
+const calculateLoad = (lines: string[]) =>
+  lines
+    .map(
+      (line, index, arr) =>
+        line.replace(new RegExp("(\\.|\\#)", "g"), "").length *
+        (arr.length - index)
+    )
+    .reduce((prev, curr) => prev + curr, 0);
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
   let lines = input.split("\n");
-  let movedSomething = false;
-
-  [movedSomething, lines] = rollRocksUp(lines);
-
-  while (movedSomething) {
-    [movedSomething, lines] = rollRocksUp(lines);
-  }
-
-  return lines
-    .map((line, index, arr) => {
-      return (
-        line.replace(new RegExp("(\\.|\\#)", "g"), "").length *
-        (arr.length - index)
-      );
-    })
-    .reduce((prev, curr) => prev + curr, 0);
+  rollRocksNorth(lines);
+  return calculateLoad(lines);
 };
 
-const spin = (lines: string[]): string[] => {
-  let newLines = [...lines];
-  let movedSomething = false;
-
-  do {
-    [movedSomething, newLines] = rollRocksUp(newLines);
-  } while (movedSomething);
-
-  do {
-    [movedSomething, newLines] = rollRocksWest(newLines);
-  } while (movedSomething);
-
-  do {
-    [movedSomething, newLines] = rollRocksDown(newLines);
-  } while (movedSomething);
-
-  do {
-    [movedSomething, newLines] = rollRocksEast(newLines);
-  } while (movedSomething);
-
-  return newLines;
-};
-
-const getKey = (lines: string[]) => {
-  return lines
-    .map((l) => {
-      if (rowCache[l] !== undefined) {
-        return rowCache[l];
-      } else {
-        rowCacheNum++;
-        rowCache[l] = rowCacheNum;
-        return rowCacheNum;
-      }
-    })
-    .reduce((prev, curr) => `${prev}_${curr}`, "");
-};
-
-let rowCacheNum = 0;
-let rowCache = {};
 let cache = {};
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
   let lines = input.split("\n");
-  let i = 1;
-  let a = 0;
-  while (i <= 1000000000) {
-    lines = spin(lines);
-    let key = getKey(lines);
+  let iteration = 1;
+  let startOfLoop = 0;
+  while (iteration <= 1000000000) {
+    rollRocksNorth(lines);
+    rollRocksWest(lines);
+    rollRocksSouth(lines);
+    rollRocksEast(lines);
+    let key = lines.join("_");
     if (cache[key] != null) {
-      console.log("repeat", cache[key], i);
-      //console.log(cache);
-      a = cache[key];
+      startOfLoop = cache[key];
       break;
     } else {
-      cache[key] = i;
+      cache[key] = iteration;
     }
 
-    i++;
+    iteration++;
   }
 
-  console.log(((1000000000 - a) % (i - a)) + a);
-  let b = ((1000000000 - a) % (i - a)) + a;
+  let iterationOfOneBillionthStep =
+    ((1000000000 - startOfLoop) % (iteration - startOfLoop)) + startOfLoop;
 
-  Object.keys(cache).forEach((k) => {
-    if (cache[k] === b) {
-      lines = k
-        .substring(1)
-        .split("_")
-        .map((n) => Number(n))
-        .map((n) => {
-          const rowKeys = Object.keys(rowCache);
-          for (let r = 0; r < rowKeys.length; r++) {
-            if (rowCache[rowKeys[r]] === n) {
-              return rowKeys[r];
-            }
-          }
-          console.log("row not found!");
-          return "";
-        });
+  const keys = Object.keys(cache);
+  for (let k = 0; k < keys.length; k++) {
+    if (cache[keys[k]] === iterationOfOneBillionthStep) {
+      return calculateLoad(keys[k].split("_"));
     }
-  });
-
-  return lines
-    .map((line, index, arr) => {
-      return (
-        line.replace(new RegExp("(\\.|\\#)", "g"), "").length *
-        (arr.length - index)
-      );
-    })
-    .reduce((prev, curr) => prev + curr, 0);
+  }
 };
 
 run({
