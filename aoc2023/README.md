447de2f1e4568ecbea0d245db80ed33c441f02d7
Mitchell Slavik <mitchell.slavik@aviture.us.com>
2023-12-20 09:17:15 -0600

    Day 20 complete

--- aoc2023/README.md
+++ aoc2023/README.md
@@ -30,7 +30,7 @@
 [![Day](https://badgen.net/badge/17/%E2%98%85%E2%98%85/green)](src/day17)
 [![Day](https://badgen.net/badge/18/%E2%98%85%E2%98%85/green)](src/day18)
 [![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
-![Day](https://badgen.net/badge/20/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%85/green)](src/day20)
 ![Day](https://badgen.net/badge/21/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
@@ -202,9 +202,9 @@ Both parts: 10.42ms
 
 ```
 Day 20
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 11.261ms
+Time part 2: 119.439ms
+Both parts: 130.699ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 38/50
-Total time: 28726.767ms
+Total stars: 40/50
+Total time: 28857.466ms
 ```
 
 <!--/RESULTS-->
---
+++ aoc2023/src/day20/index.ts
@@ -0,0 +1,224 @@
+import run from "aocrunner";
+
+interface ModuleBase {
+  id: string;
+  outputIds: string[];
+  inputIds: string[];
+}
+
+interface BasicModule extends ModuleBase {
+  type: "basic";
+}
+
+interface FlipFlopModule extends ModuleBase {
+  type: "flipflop";
+  on: boolean;
+}
+
+interface ConjunctionModule extends ModuleBase {
+  type: "conjunction";
+  inputs: Record<string, PulseStrength>;
+}
+
+type Module = FlipFlopModule | ConjunctionModule | BasicModule;
+
+type PulseStrength = "high" | "low";
+
+interface Pulse {
+  strength: PulseStrength;
+  from: string;
+  to: string;
+}
+
+const parseInput = (rawInput: string) =>
+  rawInput
+    .split("\n")
+    .map<Module>((line) => {
+      const [typeId, outputs] = line.split(" -> ");
+
+      if (typeId.startsWith("%")) {
+        const id = typeId.substring(1);
+        return {
+          id,
+          outputIds: outputs.split(", "),
+          on: false,
+          type: "flipflop",
+          inputIds: [],
+        };
+      } else if (typeId.startsWith("&")) {
+        const id = typeId.substring(1);
+        return {
+          id,
+          outputIds: outputs.split(", "),
+          inputs: {},
+          type: "conjunction",
+          inputIds: [],
+        };
+      } else {
+        return {
+          type: "basic",
+          id: typeId,
+          outputIds: outputs.split(", "),
+          inputIds: [],
+        };
+      }
+    })
+    .map((module, i, arr) => {
+      module.outputIds.forEach((outId) => {
+        const mod = arr.find((v) => v.id === outId);
+        if (mod && mod.type === "conjunction") {
+          mod.inputs[module.id] = "low";
+        }
+        if (mod) {
+          mod.inputIds.push(module.id);
+        }
+      });
+      return module;
+    })
+    .reduce((prev, curr) => {
+      prev[curr.id] = curr;
+      return prev;
+    }, {} as Record<string, Module>);
+
+const handleModulePulse = (module: Module, pulse: Pulse) => {
+  if (module.type === "basic") {
+    return module.outputIds.map<Pulse>((outId) => ({
+      from: module.id,
+      strength: pulse.strength,
+      to: outId,
+    }));
+  } else if (module.type === "flipflop") {
+    if (pulse.strength === "low") {
+      module.on = !module.on;
+      return module.outputIds.map<Pulse>((outId) => ({
+        from: module.id,
+        strength: module.on ? "high" : "low",
+        to: outId,
+      }));
+    }
+  } else if (module.type === "conjunction") {
+    module.inputs[pulse.from] = pulse.strength;
+
+    const strength: PulseStrength = Object.keys(module.inputs).some(
+      (k) => module.inputs[k] === "low"
+    )
+      ? "high"
+      : "low";
+
+    return module.outputIds.map<Pulse>((outId) => ({
+      from: module.id,
+      strength,
+      to: outId,
+    }));
+  }
+  return [];
+};
+
+const part1 = (rawInput: string) => {
+  const modules = parseInput(rawInput);
+
+  const queue: Pulse[] = [];
+  let highPulses = 0;
+  let lowPulses = 0;
+
+  let repeats = 0;
+  while (repeats < 1000) {
+    queue.push({ from: "button", strength: "low", to: "broadcaster" });
+
+    while (queue.length) {
+      const pulse = queue.shift();
+
+      if (pulse.strength === "high") {
+        highPulses++;
+      } else {
+        lowPulses++;
+      }
+
+      const module = modules[pulse.to];
+
+      if (module) {
+        queue.push(...handleModulePulse(module, pulse));
+      }
+    }
+
+    repeats++;
+  }
+
+  return highPulses * lowPulses;
+};
+
+const gcd = (a: number, b: number) => (!b ? a : gcd(b, a % b));
+
+const lcm = (a: number, b: number) => (a * b) / gcd(a, b);
+
+const resetModules = (modules: Record<string, Module>) => {
+  Object.keys(modules).forEach((key) => {
+    const module = modules[key];
+    if (module.type === "conjunction") {
+      Object.keys(module.inputs).forEach((k) => {
+        module.inputs[k] = "low";
+      });
+    } else if (module.type === "flipflop") {
+      module.on = false;
+    }
+  });
+};
+
+const part2 = (rawInput: string) => {
+  const modules = parseInput(rawInput);
+
+  const lastModule =
+    modules[
+      Object.keys(modules).find((key) => modules[key].outputIds.includes("rx"))
+    ];
+
+  return lastModule.inputIds
+    .map((subSectionId) => {
+      resetModules(modules);
+      let queue: Pulse[] = [];
+
+      let buttonPushes = 0;
+      while (true) {
+        buttonPushes++;
+        queue.push({ from: "button", strength: "low", to: "broadcaster" });
+
+        while (queue.length) {
+          const pulse = queue.shift();
+
+          if (pulse.to === subSectionId && pulse.strength === "low") {
+            return buttonPushes;
+          }
+
+          const module = modules[pulse.to];
+
+          if (module) {
+            queue.push(...handleModulePulse(module, pulse));
+          }
+        }
+      }
+    })
+    .reduce(lcm, 1);
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
